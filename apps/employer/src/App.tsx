2614346a74027602820f704bbadabcd92b30cd9e
@enwerro <rock.enwerem@tecnotree.com>
2025-02-01 08:27:23 +0100

    Update github action

--- apps/employer/src/App.tsx
+++ apps/employer/src/App.tsx
@@ -17,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React exited</h1>
 
       <Button appName="Vite">
         From library updated rea list edit all the way
